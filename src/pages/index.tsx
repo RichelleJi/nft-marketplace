7d946125aa875113e0c76101081a7e6190319d11
richelleji <ji.richelle@gmail.com>
2023-04-06 22:12:01 -0700

    Add the Hero component the component is the landing page of the app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,16 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
   return (
-   <Header/>
+    <>
+      <Header/>
+      <Hero />
+    </>
   )
 }
