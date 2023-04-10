556dd16e2f173e4cd8917fc01eb051d2d1e03817
richelleji <ji.richelle@gmail.com>
2023-04-10 04:36:10 -0700

    Clean up unused imports in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Web3Provider } from '../../providers/Web3'
-import { useIsMounted } from '../../hooks/useIsMounted'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const isMounted = useIsMounted()
 
   return (
       <Web3Provider>
-        {isMounted && (
-          <Component {...pageProps} />
-        )}
+        <Component {...pageProps} />
       </Web3Provider>
   )
 }
